af94ed960f2723d830048cf022530c6b5a7a51e1
d2nst <wlsdl950@gmail.com>
2023-04-17 11:19:46 +0900

    적응형 완료

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const scroller = document.querySelector('.header');
 const bannerOut = document.querySelector('.banner');
 const searchIcon = document.querySelector('.search');
-const searchActive = document.querySelector('.search_bg');
-const searchClose = document.querySelector('.search_bg i');
+const searchActive = document.querySelector('.search__bg');
+const searchClose = document.querySelector('.search__bg i');
 
 searchIcon.addEventListener('click', () => {
   searchActive.classList.toggle('active');
@@ -52,4 +52,39 @@ const newSwiper = new Swiper('.new-swiper', {
     el: '.swiper-pagination',
     type: 'progressbar',
   },
+});
+
+// product.html - 상세페이지 펼쳐보기
+window.onload = function () {
+  // 초기 이미지 높이 설정
+
+  const detail = document.querySelector('.detail');
+  detail.style.height = '900px';
+
+  // 상세보기 버튼 클릭 시 이미지 높이 전체/1000px로 설정
+  document.querySelector('.btn-open').addEventListener('click', function (e) {
+    e.preventDefault();
+    const detail = document.querySelector('.detail');
+    const currentHeight = parseInt(window.getComputedStyle(detail).height);
+    const newHeight =
+      currentHeight === 900
+        ? parseInt(
+            window.getComputedStyle(detail.querySelector('.content')).height
+          )
+        : 1000;
+    detail.style.transition = 'height 0.5s ease-in-out';
+    detail.style.height = newHeight + 'px';
+
+    const scrollY = window.scrollY;
+    window.scrollTo(0, scrollY);
+  });
+};
+
+// product.html - rating__bar
+const progressBars = document.querySelectorAll('.progress-bar');
+
+progressBars.forEach((progressBar) => {
+  const targetRate = progressBar.getAttribute('data-num');
+  const progressValue = progressBar.querySelector('.progress-value');
+  progressValue.style.width = `${targetRate}%`;
 });
