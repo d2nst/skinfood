a8e92b4118a5849b32247642a2a4d1b78635f7d6
d2nst <wlsdl950@gmail.com>
2023-04-19 19:06:58 +0900

    Community 반응형

--- js/main.js
+++ js/main.js
@@ -31,12 +31,36 @@ window.addEventListener('scroll', () => {
 
 // swiper JS
 const swiper = new Swiper('.swiper', {
-  spaceBetween: 50, // 슬라이드 사이 여백
-  slidesPerView: 3, // 한 슬라이드에 보여줄 갯수
   autoplay: {
     delay: 4000,
   },
   loop: true,
+
+  // Default parameters
+  spaceBetween: 50, // 슬라이드 사이 여백
+  slidesPerView: 3, // 한 슬라이드에 보여줄 갯수
+  // Responsive breakpoints
+  breakpoints: {
+    1170: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+  },
 });
 
 const newSwiper = new Swiper('.new-swiper', {
@@ -52,6 +76,27 @@ const newSwiper = new Swiper('.new-swiper', {
   pagination: {
     el: '.swiper-pagination',
     type: 'progressbar',
+  },
+
+  breakpoints: {
+    1170: {
+      spaceBetween: 30, // 슬라이드 사이 여백
+      slidesPerView: 5,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
   },
 });
 
